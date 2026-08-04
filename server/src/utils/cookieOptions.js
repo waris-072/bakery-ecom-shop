42f9cd8cb7e663eba0490918007ec3a7ddd8ed71
waris-072 <warishakrow3@gmail.com>
2026-08-04 17:08:01 +0500

    fix: enable cross-site cookies for production

--- server/src/utils/cookieOptions.js
+++ server/src/utils/cookieOptions.js
@@ -1,7 +1,10 @@
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
+  sameSite:
+    process.env.NODE_ENV === "production"
+      ? "none"
+      : "lax",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
